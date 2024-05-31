53c5eff117b16d406450251c21e303c8c94ed679
Chandan Sahu <chandan.sahu@bonamisoftware.com>
2024-05-31 14:54:39 +0530

    FEAT: Footer and LetsConnect component added, removed chakra

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex min-w-full justify-around p-6 items-center bg-[#131313] text-[#797979E8] text-sm font-semibold ">
+      <span>Copyright: © 2024 | All Rights Reserved</span>
+      <span>Privacy Policy</span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+import LetsConnect from "@/components/LetsConnect";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
       Hello
+      <LetsConnect/>
+      <Footer/>
     </main>
   );
 }
