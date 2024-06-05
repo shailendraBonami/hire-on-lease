eed7e115d41262e506ca98c86d4c12998d055d8b
Nishant Fulara <nishantfulara@gmail.com>
2024-06-05 17:48:13 +0530

    add tabs section in about page
    added plugin for gradient deg
    
    route modification
    
    refactor code

---
+++ src/utils/WhyChooseUs.js
@@ -0,0 +1,37 @@
+import * as Network from "../../public/assets/10230376.png";
+
+export const tabsData = [
+  {
+    title: "Expertise",
+    url: Network,
+    desc: "years of industry experience and a proven track record of success.",
+  },
+  {
+    title: "Quality",
+    url: Network,
+    desc: "Rigorous screening to ensure top candidates",
+  },
+  {
+    title: "Flexibility",
+    url: Network,
+    desc: "Tailored solutions for your business needs",
+  },
+  {
+    title: "Support",
+    url: Network,
+    desc: "Dedicated assistance throughout and beyond the hiring process",
+  },
+  {
+    title: "Network",
+    url: Network,
+    desc: "Extensive professional network across various industries.",
+  },
+];
+
+export const TEXTS = {
+  TITLE: "Why Choose HireOnLease",
+  AT: "At",
+  HIRE_ON_LEASE: "HireOnLease",
+  DESCRIPTION:
+    ", we are committed to providing exceptional staffing solutions. Here’s why you should choose us.",
+};
---
+++ src/utils/banner.js
@@ -0,0 +1,17 @@
+export const bannerJson = [
+  {
+    label: "About Us",
+    breadCrumbLabel: "About",
+    imageUrl: "/assets/aboutBanner.svg",
+  },
+  {
+    label: "Services",
+    breadCrumbLabel: "Services",
+    imageUrl: "/assets/serviceBanner.svg",
+  },
+  {
+    label: "Contact Us",
+    breadCrumbLabel: "Contact us",
+    imageUrl: "/assets/contactBanner.svg",
+  },
+];
